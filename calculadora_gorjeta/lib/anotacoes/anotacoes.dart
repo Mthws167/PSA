d82c039f3d279084dcdff529c4a040f5093bea50
Mthws167 <mthws.henrique@hotmail.com>
2023-04-14 20:25:48 -0300

    anotações

--- calculadora_gorjeta/lib/anotacoes/anotacoes.dart
+++ calculadora_gorjeta/lib/anotacoes/anotacoes.dart
@@ -10,4 +10,16 @@
  * Abstração: na classe Cliente, são definidos atributos como "nome", "avaliacao" e "gorjeta", que representam as informações essenciais de um cliente no contexto do sistema.
  * 
  * 
+ * 
+ * SOLID:
+ * 
+ * SRP (Single Responsibility Principle): uma classe deve ter apenas uma única responsabilidade.
+ * 
+ * OCP (Open-Closed Principle): uma vez que permitem a extensão sem modificação da classe "Atendente".
+ * 
+ * LSP (Liskov Substitution Principle): afirma que as classes filhas devem ser substituíveis pelas classes pais. Isso pode ser aplicado na classe "AtendenteTemporario" e "AtendenteFixo", que são classes filhas de "Atendente".
+ * 
+ * ISP (Interface Segregation Principle):
+ * 
+ * DIP (Dependency Inversion Principle):  
  * */
